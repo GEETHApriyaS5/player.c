a32eb4e0423601c51c89425dae74406746c76489
GEETHApriyaS5 <46866498+GEETHApriyaS5@users.noreply.github.com>
2019-02-16 02:56:48 -0800

    Create roman.c

---
+++ roman.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int V=5,I=1,X=10,i,c=0,d=0,e=0,g=0;
+    char a[100];
+    scanf("%s",a);
+    for(i=0;a[i]!='\0';i++)
+    {
+        if(a[i]='I')
+        {
+            if(a[i+1]=='V')
+            {
+                c=4;
+                i++;
+            }
+            else if(a[i+1]=='X')
+            {
+                c=9;
+                i++;
+            }
+            else
+            {
+                c=c+1;
+            }
+        }
+       else
+        {
+            if(a[i]=='V')
+            {
+                d=d+5;
+                
+            }
+            if(a[i]=='X')
+            {
+                e=e+10;
+            }
+        }
+    }
+
+        g=c+d+e;
+        printf("%d",g);
+        getch();
+    }
