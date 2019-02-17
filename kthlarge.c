bc1b7c5d58b26bcfdfa2578f2ba51efc882f6cb9
GEETHApriyaS5 <46866498+GEETHApriyaS5@users.noreply.github.com>
2019-02-17 21:50:03 +0530

    Create kthlarge.c

---
+++ kthlarge.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a[100],k,i,j,t,n;
+scanf("%d%d",&n,&k);
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<n;i++)
+{
+for(j=0;j<n-1;j++)
+{
+if(a[i]>a[j+1])
+{
+t=a[j];
+a[j]=a[j+1];
+a[j+1]=t;
+}
+}}
+printf("%d",a[k-1]);
+getch();
+}
