ffb196e8b76c6e37108b916dea287549770363fc
GEETHApriyaS5 <46866498+GEETHApriyaS5@users.noreply.github.com>
2019-02-19 21:30:52 +0530

    Create sp.c

---
+++ sp.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    char a[100];
+    int i;
+    scanf("%s",a);
+    
+          printf("%s",a);
+    
+    /*for(i=0;a[i]!='\0';i++)
+    {
+        //if(a[i]!='$')
+        {
+            printf("%c",a[i]);
+        }
+    }*/
+    getch();
+}
