29eefe9c50d8a216cc4aa4a3e73aec47f21fa269
GEETHApriyaS5 <46866498+GEETHApriyaS5@users.noreply.github.com>
2019-02-19 06:48:37 +0530

    Create strup.c

---
+++ strup.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    char a[100];
+    int i;
+    scanf("%s",a);
+    for(i=0;a[i]!='\0';i++)
+    {
+        if(a[i]>=65 && a[i]<=92)
+        {
+            a[i]=a[i]+32;
+        }
+        else
+        {
+            a[i]=a[i]-32;
+        }
+    }
+    printf("%s",a);
+    getch();
+}
