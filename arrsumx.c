df67d727588c987ad9b140480fb0f8d201aa9ffe
GEETHApriyaS5 <46866498+GEETHApriyaS5@users.noreply.github.com>
2019-02-26 06:46:17 +0530

    Create arrsumx.c

---
+++ arrsumx.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int n,x,a[100],i,j,c=0;
+    scanf("%d",&n);
+    scanf("%d",&x);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=i+1;j<n;j++)
+        {
+            if(a[i]+a[j]==x)
+            {
+                c++;
+            }
+        }
+    }
+    if(c>0)
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("no");
+    }
+    getch();
+}
