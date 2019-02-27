a92921b74375c6cf43edce6fdb9b049ed5a71d58
GEETHApriyaS5 <46866498+GEETHApriyaS5@users.noreply.github.com>
2019-02-27 06:31:05 +0530

    Create powerab.c

---
+++ powerab.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a,b,p=1,i;
+    scanf("%d%d",&a,&b);
+    for(i=0;i<b;i++)
+    {
+        p=p*a;
+    }
+    printf("%d",p);
+    getch();
+}
