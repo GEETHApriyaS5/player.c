e50c9380334faac40126064b703fcd467ab611e4
GEETHApriyaS5 <46866498+GEETHApriyaS5@users.noreply.github.com>
2019-03-18 20:10:46 +0530

    Create bitor.c

---
+++ bitor.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int n,a[100],i=0,j=0,m1,m=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {m1=1;
+        for(j=i+1;j<n;j++)
+        {
+            m=a[i]|a[j];
+            /*if(m>m1)
+            {
+                m1=m;
+            }*/
+        }
+    }
+    printf("%d",m);
+    getch();
+}
